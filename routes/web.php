3717668bebf222149f6429bae580fd05d4358338
Ilan ARFI <i.arfipro@outlook.fr>
2025-07-07 07:45:50 +0200

    maj secure route dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::get('dashboard', \App\Livewire\Dashboard::class)
-    ->middleware(['auth'])
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
 Route::view('profile', 'profile')
